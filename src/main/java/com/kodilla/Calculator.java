d2434648e32c4b0646395e69ea4fa9dc03c6d951
majic1985 <majic1985.mk@gmail.com>
2019-02-13 12:17:27 +0100

    First Commit

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,11 @@
+package com.kodilla;
+
+public class Calculator {
+    public void someMethod() {
+}
+public static void main (String args []) {
+    Calculator calculator = new Calculator();
+    calculator.someMethod();
+    System.out.println("It's works");
+ }
+}
