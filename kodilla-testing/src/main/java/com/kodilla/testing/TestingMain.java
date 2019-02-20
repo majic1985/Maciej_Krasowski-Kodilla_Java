d6352f6c5f4b532030596385e0237f96f42e22e8
majic1985 <majic1985.mk@gmail.com>
2019-02-20 16:20:43 +0100

    calc

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,24 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+
+            Calculator calculator = new Calculator();
+        Integer result = calculator.add(5,5);
+        Integer result1 = calculator.substract(10,8);
+
+        if (result.equals(5 + 5)){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        if (result1.equals(10 - 8)){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
+
