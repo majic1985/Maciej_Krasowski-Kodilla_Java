00fc04941fac24f9daf098324897af3d20607d4f
majic1985 <majic1985.mk@gmail.com>
2019-04-08 20:12:57 +0200

    M7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,10 +1,11 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main( String[] args ) {
-
+      //7.1
         System.out.println();
         String rawPoem = new String("Jedzą, piją, lulki palą,\nTańce, hulanka, swawola;\nLedwie karczmy nie rozwalą,\nCha cha, chi chi, hejza, hola!\n\nTwardowski siadł w końcu stoła.\nPodparł się w boki jak basza;\n\"Hulaj dusza! hulaj!\" - woła,\nŚmieszy, tumani, przestrasza.\n\nŻołnierzowi, co grał zucha,\nWszystkich łaje i potrąca,\nŚwisnął szablą koło ucha,\nJuż z żołnierza masz zająca.");
         PoemBeautifier poemBeautifier = new PoemBeautifier();
@@ -24,6 +25,9 @@ public class StreamMain {
             System.out.println("-------------------\nLambda expression #4\n");
             return Str.replace("Twardowski", "POLITYCZNIE POPRAWNY CZLOWIEK");
         });
+        //M7.2
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
 
