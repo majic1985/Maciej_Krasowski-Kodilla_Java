f97c63ee3a692a5ab7ceb8b029401ee5106b8c21
majic1985 <majic1985.mk@gmail.com>
2019-04-08 19:09:16 +0200

    7.1

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,35 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+
+public class StreamMain {
+    public static void main(String[] args) {
+    }
+            public StreamMain() {
+
+            System.out.println();
+            String rawPoem = new String("Jedzą, piją, lulki palą,\nTańce, hulanka, swawola;\nLedwie karczmy nie rozwalą,\nCha cha, chi chi, hejza, hola!\n\nTwardowski siadł w końcu stoła.\nPodparł się w boki jak basza;\n\"Hulaj dusza! hulaj!\" - woła,\nŚmieszy, tumani, przestrasza.\n\nŻołnierzowi, co grał zucha,\nWszystkich łaje i potrąca,\nŚwisnął szablą koło ucha,\nJuż z żołnierza masz zająca.");
+            PoemBeautifier poemBeautifier = new PoemBeautifier();
+            poemBeautifier.beautify(rawPoem, (Str) -> {
+                System.out.println("-------------------\nLambda expression #1\n");
+                return Str.toUpperCase();
+            });
+            poemBeautifier.beautify(rawPoem, (Str) -> {
+                System.out.println("-------------------\nLambda expression #2\n");
+                return Str.substring(110, 200);
+            });
+            poemBeautifier.beautify(rawPoem, (Str) -> {
+                System.out.println("-------------------\nLambda expression #3\n");
+                return "ABC   ->>>> " + Str + " <<<<- CBA";
+            });
+            poemBeautifier.beautify(rawPoem, (Str) -> {
+                System.out.println("-------------------\nLambda expression #4\n");
+                return Str.replace("Twardowski", "POLITYCZNIE POPRAWNY CZLOWIEK");
+            });
+        }
+    }
+
+
+
+
+
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -0,0 +1,13 @@
+package com.kodilla.stream.beautifier;
+
+public class PoemBeautifier {
+    public PoemBeautifier() {
+    }
+
+    public void beautify(String Str, PoemDecorator stringToDecorate) {
+        String result = stringToDecorate.decorate(Str);
+        result = "*** Pani Twardowska ***\n" + result + "\n...to be continued";
+        System.out.println(result);
+    }
+}
+
