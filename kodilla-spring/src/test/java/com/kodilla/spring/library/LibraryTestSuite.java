be8f8dea84f06a32731c14c39f30091335ccd0f5
majic1985 <majic1985.mk@gmail.com>
2019-04-26 11:05:33 +0200

    Library

--- kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -8,30 +8,29 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-    @SpringBootTest
-    public class LibraryTestSuite {
-        @Test
-        public void testLoadFromDb() {
-            //Given
-            ApplicationContext context =
-                    new AnnotationConfigApplicationContext("com.kodilla.spring");
-            Library library = context.getBean(Library.class);
-            //When
-            library.loadFromDb();
-            //Then
-            //do nothing
-        }
-
-        @Test
-        public void testSaveToDb() {
-            //Given
-            ApplicationContext context =
-                    new AnnotationConfigApplicationContext("com.kodilla.spring");
-            Library library = context.getBean(Library.class);
-            //When
-            library.saveToDb();
-            //Then
-            //do nothing
-        }
+@SpringBootTest
+public class LibraryTestSuite {
+    @Test
+    public void testLoadFromDb() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Library library = context.getBean(Library.class);
+        //When
+        library.loadFromDb();
+        //Then
+        //do nothing
     }
 
+    @Test
+    public void testSaveToDb() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Library library = context.getBean(Library.class);
+        //When
+        library.saveToDb();
+        //Then
+        //do nothing
+    }
+}
