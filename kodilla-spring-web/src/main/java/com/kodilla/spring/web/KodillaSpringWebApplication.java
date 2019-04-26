77985f081aae659fa9f5ab9cdc966992535cc049
majic1985 <majic1985.mk@gmail.com>
2019-04-26 12:28:23 +0200

    14.4

--- kodilla-spring-web/src/main/java/com/kodilla/spring/web/KodillaSpringWebApplication.java
+++ kodilla-spring-web/src/main/java/com/kodilla/spring/web/KodillaSpringWebApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KodillaSpringWebApplication {
+public class KodillaSpringWebApplication  {
 
     public static void main( String[] args ) {
         SpringApplication.run(KodillaSpringWebApplication.class, args);
